31841886453ee5a64fe4bf636f4b52bc6d39b7cc
Breno Gabriel Antunes dos Santos <breno.antunes@pucpr.edu.br>
2026-04-08 11:09:06 -0300

    Conectando banco virtual

--- src/Views/doacao.php
+++ src/Views/doacao.php
@@ -1,25 +1,28 @@
 <?php
-
-$host = '127.0.0.1';
-$port = '3307';
+$host = 'gateway01.us-east-1.prod.aws.tidbcloud.com';
+$port = '4000';
 $db   = 'grative';
-$user = 'root';
-$pass = 'breno136';
+$user = '3ECGGvFuYA5vMsA.root';
+$pass = 'o9siRzf71pKIzfap';
 
+$status = "";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $valor = $_POST['valor'] ?? 0;
     $msg   = $_POST['mensagem'] ?? '';
 
     try {
-       
-        $pdo = new \PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $user, $pass);
+        $options = [
+            PDO::MYSQL_ATTR_SSL_CA => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
         
-        $sql = "INSERT INTO donations (amount, message) VALUES (?, ?)";
+         $pdo = new \PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $user, $pass, $options); 
+        
+        $sql = "INSERT INTO doacao (quantia, forma_pagan, fk_id_usuaria, fk_id_institui) VALUES (?, ?, 1, 1)";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$valor, $msg]);
 
-       
         header("Location: doacao.php?ok=" . $valor);
         exit;
         
@@ -28,12 +31,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-
-$status = "";
 if (isset($_GET['ok'])) {
-    
+    $status = " Sucesso! R$ " . $_GET['ok'] . " gravado no banco.";
 } elseif (isset($erro)) {
-    
+    $status = "erro " . $erro;
 }
 ?>
 <!DOCTYPE html>
@@ -46,7 +47,6 @@ if (isset($_GET['ok'])) {
 
     <h2>Fazer Doação</h2>
     
-    
     <?php if ($status): ?>
         <p style="color: blue; font-weight: bold; border: 1px solid blue; padding: 10px;">
             <?php echo $status; ?>
@@ -57,7 +57,7 @@ if (isset($_GET['ok'])) {
         <p>Valor (R$):<br>
         <input type="number" name="valor" step="0.01" required></p>
 
-        <p>Mensagem:<br>
+        <p>Forma de Pagamento / Mensagem:<br>
         <input type="text" name="mensagem"></p>
 
         <button type="submit" style="padding: 10px; cursor: pointer;">ENVIAR</button>
