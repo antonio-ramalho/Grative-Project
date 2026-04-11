0ecd3b956df11b9f0c06ef0778927830abf5019b
antonio <antoninho.ferreira2015@gmail.com>
2026-04-11 00:13:04 -0300

    Voltar para o original

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,8 +1,4 @@
 <?php
-//OSC rotas
-require_once __DIR__ . '/../Controllers/OscController.php';
-$router->get('/cadastro', 'OscController@mostrarFormulario');
-
 // src/Routes/web.php — definição de rotas
 $router->get('/', 'HomeController@index');
 $router->get('/relatorio-doacoes', 'DonationReportController@index');
